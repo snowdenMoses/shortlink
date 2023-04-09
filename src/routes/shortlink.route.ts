9a443c39f0414fb14fdf35e93ab4b0a2ee95b0d7
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-09 12:39:25 +0100

    creates a shortlink route

---
+++ src/routes/shortlink.route.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { IRoutes } from '../interface/route.interface';
+import ShortLinkController from '../controller/shortlink.controller';
+
+class ShortLinkRoute implements IRoutes {
+    public path = '/';
+    public router = Router();
+    private shortLinkController = new ShortLinkController();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes() {
+        this.router.get(`${this.path}/:short_url`, this.shortLinkController.redirectShortUrlToLongUrl);
+        this.router.post(`${this.path}/encode`, this.shortLinkController.encodeURL);
+        this.router.post(`${this.path}/decode`, this.shortLinkController.decodeURL);
+        this.router.post(`${this.path}/statistics/:short_url`, this.shortLinkController.getShortUrlStatistics);
+    }
+}
+export default ShortLinkRoute;
