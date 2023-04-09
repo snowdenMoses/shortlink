dd64d4a1d76100ac7088c9fb1fb0d51ad5ebcb96
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-09 12:08:45 +0100

    creates a short link service

--- src/interface/shortlink.interface.ts
+++ src/interface/shortlink.interface.ts
@@ -1,4 +1,14 @@
 export interface IShortLink{
-    id: string;
+    generated_string: string;
     url: string
+}
+
+export interface IEncode {
+    short_url: string;
+}
+export interface IDecode {
+    long_url?: string
+}
+export interface IStatistics {
+    originally_gotten_from?: string;
 }
---
+++ src/service/shortlink.service.ts
@@ -0,0 +1,29 @@
+import { IShortLink, IEncode, IDecode, IStatistics } from "../interface/shortlink.interface";
+import ShortStringGenerator from "../helperMethods/short_string_generator";
+
+class ShortLinkService{
+    private port = process.env.PORT || 3000
+    private array_of_urls: IShortLink[] = []
+    private shortStringGenerator = new ShortStringGenerator()
+
+    public async encodeURL(url: string): Promise<IEncode> {
+            const generated_string = this.shortStringGenerator.generateString()
+            this.array_of_urls.push({ generated_string, url });
+            const short_url = `http://localhost:${this.port}/${generated_string}`;
+            return { short_url }
+    }
+
+    public async decodeURL(short_url: string): Promise<IDecode> {
+            const url: IShortLink | undefined = this.array_of_urls.find(url => url.generated_string === short_url);
+            const long_url = url?.url
+            return { long_url }
+       
+    }
+    public async getShortUrlStatistics(short_url: string): Promise<IStatistics> {
+            const url = this.array_of_urls.find(url => url.generated_string === short_url);
+            const long_url = url?.url
+            return { originally_gotten_from: long_url }
+       
+    }
+}
+export default ShortLinkService
