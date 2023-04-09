e5b3126d10737a91b100c25c5f8574931c38a3e2
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-04-09 11:34:07 +0100

    creates a random string generator helper method

---
+++ src/helperMethods/short_string_generator.ts
@@ -0,0 +1,8 @@
+class ShortStringGenerator{
+    public generateString(){
+        const randomString = Math.random().toString(36).slice(2, 8)
+        return randomString
+    }
+}
+
+export default ShortStringGenerator
